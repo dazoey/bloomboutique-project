d3290290f3d7b7c8272913c87ca215d0d57d4871
dazoey <athayaarch@gmail.com>
2025-05-01 17:14:52 +0700

    router-update

---
+++ src/router/main.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import App from '../App.vue'
+import PaymentPage from '../views/PaymentPage.vue'
+import HomePage from '../views/HomePage.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HomePage
+  },
+  {
+    path: '/Payment',
+    name: 'Payment',
+    component: PaymentPage
+  },
+  {
+    path: '/HomePage',
+    name: 'HomePage',
+    component: HomePage
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
