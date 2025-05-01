d64f3b44a56c9d4410eac4f0535660d88e97cc13
dazoey <athayaarch@gmail.com>
2025-05-02 00:28:46 +0700

    elementor update

--- src/components/body/BodyComponent.vue
+++ src/components/body/BodyComponent.vue
@@ -1,8 +1,18 @@
-<script setup>
+<script >
 import BodyElementor from '../brand/BodyElementor.vue';
 import ExperienceBodyComponent from '../experience/ExperienceBodyComponent.vue';
 import FooterCard from '../footer/FooterCard.vue';
 import BungaCard from '../bunga/BungaCard.vue';
+
+export default{
+  name: 'BodyComponent',
+  components: {
+    BodyElementor,
+    ExperienceBodyComponent,
+    FooterCard,
+    BungaCard
+  }
+}
 
 </script>
 
@@ -24,6 +34,6 @@ import BungaCard from '../bunga/BungaCard.vue';
 </div>
 
 </template>
-<style scoped>
+<style >
 
 </style>
